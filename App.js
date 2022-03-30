ad5c726ee819f0c055a30e5dc09edd5d217037d8
Gilbert <gssenyonjo@gmail.com>
2022-03-30 19:26:31 +0300

    used react  state

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React , { useState } from 'react'
+import { Text, Image, Button, ScrollView, View, TextInput } from 'react-native';
 
 const MyApp = () => {
+    const [clicked, setClicked] = useState(false)
     return (
-        <View style={{ flext: 1, justifyContent: 'center', alignItems: 'center'}} >
-            <Text>
-                Try editing me, pal!
-            </Text>
-        </View>
+        <ScrollView>
+            <Text>Hey there!</Text>
+            <View>
+                { clicked &&
+                    <Text>How are you doing?</Text>
+                }   
+            </View>
+            <TextInput
+                style={{
+                    height: 40,
+                    borderColor: 'pink',
+                    borderWidth: 2
+                }}
+                defaultValue="Tell me about your day"
+                >
+
+            </TextInput>
+
+            <Button 
+                title={'Great button'}
+                onPress={()=> {setClicked(true)}}
+                />
+
+        </ScrollView>
     )
 }
 
